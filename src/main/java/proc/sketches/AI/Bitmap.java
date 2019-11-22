a94edda284bdc1116f54d8b0711aeb96599027da
atOliverParkerMorgan <oliver.parker.morgan@gmail.com>
2019-11-23 00:04:14 +0100

    comments

--- src/main/java/proc/sketches/AI/Bitmap.java
+++ src/main/java/proc/sketches/AI/Bitmap.java
@@ -5,12 +5,40 @@ import proc.sketches.Grid.Spot;
 import proc.sketches.Shapes.Shape;
 
 public class Bitmap {
+    /*
+    Bitmap stores all grid information in simplified array
+    1) if there is a block occupying a spot it is represented as a 1
+    2) if these is spot that isn't occupied by a block than it is represented by a 0
 
+    *
+    in this bitmap the moving shape is just an array of coordinates (or indexes according the bitmap) of the blocks
+    the block that has the smallest x and y values (is the closest to the top left corner) is the first cords in this
+    array !but only in the default structure! this doesn't have to apply if the shape rotates
+    *
+
+    (the rotation has it's own rules):
+    1) the array that contains zero values is the center axis of the rotation
+    2) each rotation depend on the values of previous rotation
+
+    we can do these operation with the moving shape:
+    1) rotate
+    2) move down => move down until collision
+    3) move up => move up to until any block of the shape has the value: {y = 0}
+    4) move left => move left to until any block of the shape has the value: {x = 0}
+    5) move right
+
+     */
+
+
+    // bitmap values
     int[][] Bitmap;
     private int[][] movingShapeCords;
     private int categoryOfMovingShape;
+    // number of possible rotation states => 2, 3, 4
     private int state = 0;
 
+
+    // different rotation positions for different shapes
     private static final int[][] I1 = new int[][]{{-1,1},{0,0},{1,-1},{2, -2}};
     private static final int[][] I2 = new int[][]{{1,-1},{0,0},{-1,1},{-1*2, 2}};
     private static final int[][][] IALL = new int[][][]{I2,I1};
@@ -41,6 +69,7 @@ public class Bitmap {
     private static final int[][] Z2 = new int[][]{{0, -2},{1,-1},{0,0},{1,1}};
     private static final int[][][] ZALL = new int[][][]{Z1,Z2};
 
+    // an array that contains all rotation cords (so it's easier to access them in a for loop)
     private static final int[][][][] ALLALL = new int[][][][]{IALL,LALL,SALL,JALL,TALL,ZALL};
 
 
@@ -54,6 +83,7 @@ public class Bitmap {
 
     }
     void resetBitmap(){
+        // set all bitmap values to 1 or unoccupied
         for(int y=0;y<Shape.getNum_Y();y++){
             for(int x=0;x<Shape.getNum_X();x++){
                 Bitmap[y][x] = 1;
@@ -65,6 +95,9 @@ public class Bitmap {
 
 
     private void moveToCenter(){
+        // this method is accessed so when the shape rotates it doesn't overlap the canvas
+
+        // the space between the top of the canvas and the shape
         int bound = 2;
 
         while (true) {
@@ -83,7 +116,7 @@ public class Bitmap {
     }
 
     void syncBitmapWithGrid(Spot[][] Grid, Shape movingShape){
-
+        // sets all values in bitmap to reflect the Grid boolean occupied values
 
         for (int y = 0; y < Shape.getNum_Y(); y++) {
             for (int x = 0; x < Shape.getNum_X(); x++) {
@@ -94,10 +127,12 @@ public class Bitmap {
                 }
             }
         }
+        // delete the values of the original movingShape
         for(Block block: movingShape.getAllblocks()){
             Bitmap[(int) block.y/Shape.getSIZE()][(int) block.x/Shape.getSIZE()] = 1;
         }
 
+        // add the the values of the bitmap movingShape cords
         for (int[] movingShapeCord : movingShapeCords) {
 
             Bitmap[movingShapeCord[1]][movingShapeCord[0]] = 0;
@@ -110,6 +145,7 @@ public class Bitmap {
 
 
     void moveMovingShapeDown(){
+        // move shape down until collision
         mainLoop:
         while (true){
             for(int[] cords : movingShapeCords){
@@ -141,6 +177,7 @@ public class Bitmap {
 
     }
     private void moveMovingShapeLeft(){
+        // move shape left and update values (set old spot to 1 and new to 0)
         for(int i=0;i<movingShapeCords.length;i++){
             Bitmap[movingShapeCords[i][1]][movingShapeCords[i][0]] = 1;
             movingShapeCords[i][0] --;
@@ -149,6 +186,7 @@ public class Bitmap {
 
     }
     void moveMovingShapeRight(){
+        // move shape right and update values
         for(int i=0;i<movingShapeCords.length;i++){
             Bitmap[movingShapeCords[i][1]][movingShapeCords[i][0]] = 1;
             movingShapeCords[i][0] ++;
@@ -156,6 +194,7 @@ public class Bitmap {
         }
     }
     private void moveDown(){
+        // move shape down and update values
         for(int i=0;i<movingShapeCords.length;i++){
             Bitmap[movingShapeCords[i][1]][movingShapeCords[i][0]] = 1;
             movingShapeCords[i][1] ++;
@@ -164,10 +203,12 @@ public class Bitmap {
     }
 
     int getBit(int x, int y){
+        // get the bit in the array
         return Bitmap[y][x];
 
     }
     void moveShapeLeft(){
+        // move the shape all the way to the left
         mainLoop:
         while (true){
             for (int[] cord:movingShapeCords) {
@@ -180,6 +221,7 @@ public class Bitmap {
     }
 
     int getNumberOfCycles(){
+        // get the number of cycles needed in a for loop to go through all the possible possibilities (from left to right)
         int biggestX = Integer.MIN_VALUE;
 
         for (int[] cord:movingShapeCords) {
@@ -193,6 +235,7 @@ public class Bitmap {
     }
 
     public void rotate(){
+        // rotate shape and update the state
         if(this.categoryOfMovingShape!=6) {
             moveToCenter();
             for (int i = 0; i < Shape.numberOfBlocks; i++) {
@@ -242,6 +285,7 @@ public class Bitmap {
     }
 
     void moveMovingShapeUp(){
+        // move the shape to the top
         mainLoop:
         while (true){
             for(int[] cords : movingShapeCords){
@@ -254,6 +298,7 @@ public class Bitmap {
         }
     }
     private void moveUp(){
+        // move shape up and update values
         for(int i=0;i<movingShapeCords.length;i++){
             Bitmap[movingShapeCords[i][1]][movingShapeCords[i][0]] = 1;
             movingShapeCords[i][1] --;
@@ -262,6 +307,7 @@ public class Bitmap {
     }
 
     double[][] getCords(double[][] cords){
+        // get the coordinates of the moving shape
         for(int i = 0;i<4;i++ ){
             cords[i][0] = movingShapeCords[i][0];
             cords[i][1] = movingShapeCords[i][1];
